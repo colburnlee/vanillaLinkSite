fd8ed4747c9d531f4f6ad09d4c7f8ff47dd916fe
Lee Colburn <colburn.lee@gmail.com>
2022-07-22 13:45:51 -0700

    adding navbar

---
+++ README.old.md
@@ -0,0 +1,1 @@
+**_Vanilla React Link Site_**
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <div className="container">
+        <Header title="Lee" />
+      </div>
+    </>
+  );
+}
+
+export default App;
