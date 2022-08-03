bf6524c4641621323de8c3bfebc31c67363b9a32
Lee Colburn <leecolburn@Lees-MacBook-Pro.local>
2022-08-03 15:33:01 -0700

    adding brush update

--- src/components/RechartOHLCCompare.js
+++ src/components/RechartOHLCCompare.js
@@ -6,6 +6,7 @@ import {
   CartesianGrid,
   Tooltip,
   Legend,
+  Brush,
 } from "recharts";
 import * as d3 from "d3";
 
@@ -13,18 +14,18 @@ const RechartOHLCCompare = (LineChartProps = {}) => {
   
   const { width, height, data, comparisonData } = LineChartProps;
   const priceConstructor = `${data[0].description} Price`;
-  const dateOptions = {
-    // weekday: "short",
-    year: "2-digit",
-    month: "short",
-    // day: "2-digit",
-  };
+  // const dateOptions = {
+  //   // weekday: "short",
+  //   year: "2-digit",
+  //   month: "short",
+  //   // day: "2-digit",
+  // };
   //   const formatTime = (date) => {
   //     return d3.timeFormat("%B %d, %Y")(date.period);
   //   };
 
   const dateFormat = d3.timeFormat("%-H:%M %b %d, %y");
-  console.log("Time formatting test: ", dateFormat(new Date(1596931200000)));
+  // console.log("Time formatting test: ", dateFormat(new Date(1596931200000)));
 
   const lineChartData = comparisonData.data.reduce(
     (accumulator, currentValue) => {
@@ -59,19 +60,20 @@ const RechartOHLCCompare = (LineChartProps = {}) => {
   //   console.log(`X Axis Min: ${xMin}  Max: ${xMax}`);
   //   console.log(`Y Axis Min: ${yMin}  Max: ${yMax}`);
 
-  console.log("OHLC Array Sample: ", lineChartData[0]);
-  console.log("Oracle Array Sample: ", oracle_data[0]);
-  console.log("OHLC Array Length: ", lineChartData.length);
-  console.log("Oracle Array Length: ", oracle_data.length);
+  // console.log("OHLC Array Sample: ", lineChartData[0]);
+  // console.log("Oracle Array Sample: ", oracle_data[0]);
+  // console.log("OHLC Array Length: ", lineChartData.length);
+  // console.log("Oracle Array Length: ", oracle_data.length);
 
   const combinedData = lineChartData.concat(oracle_data).sort(function(a,b){
+    // TODO - Re-sort array to excise the thousands of ~1min updates during proxy version upgrades
     return new Date(a.date) - new Date(b.date);
   });
-  // console.log("Combined Array: ", combinedData)
+  console.log("Combined Array: ", combinedData)
 
 // FOR TESTING - SLICE VARIABLES
-  const combinedDataSlice = combinedData.slice(0,10000)
-  const lineChartSlice = lineChartData.slice(0,10000)
+  const combinedDataSlice = combinedData.slice(18000,19000)
+  const lineChartSlice = lineChartData.slice(18000,19000)
   let [xMin, xMax] = d3.extent(lineChartSlice, (d) => d.period);
   xMin = dateFormat(new Date(xMin));
   xMax = dateFormat(new Date(xMax));
@@ -86,7 +88,7 @@ const RechartOHLCCompare = (LineChartProps = {}) => {
       margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
     >
       <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="date" domain={[xMin, xMax]} type="category" scale={"auto"} label={{ value: "Date", position: "insideBottomRight", offset: -10 }}/>
+      <XAxis dataKey="date" domain={[xMin, xMax]} type="category" scale={"auto"} />
       <YAxis domain={[0, yMax]} label={{
           value: priceConstructor,
           angle: -90,
@@ -94,9 +96,10 @@ const RechartOHLCCompare = (LineChartProps = {}) => {
         }}/>
       <Tooltip />
       <Legend />
+      <Brush dataKey="date" height={40} stroke="#82ca9d" />
       <Line connectNulls type="monotone" dataKey="low" stroke="#8884d8" dot={false} />
       <Line connectNulls type="monotone" dataKey="high" stroke="#82ca9d" dot={false} />
-      <Line type="monotone" dataKey="oracle_price" stroke="#3d2cbf" dot={false} />
+      <Line connectNulls type="monotone" dataKey="oracle_price" stroke="#3d2cbf" dot={false} />
     </LineChart>
   );
 };
