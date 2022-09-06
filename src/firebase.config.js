8aeadefff2b017ed19fb59327637dbeccd342205
Lee Colburn <colburn.lee@gmail.com>
2022-09-06 14:55:48 -0700

    adding firebaseconfig

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getStorage, connectStorageEmulator } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_APIKEY,
-  authDomain: process.env.REACT_APP_AUTHDOMAIN,
-  projectId: process.env.REACT_APP_PROJECTID,
-  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
-  appId: process.env.REACT_APP_APPID,
-  measurementId: process.env.REACT_APP_MEASUREMENTID,
+  apiKey: "AIzaSyC4XdigZKyiQI0oAndOvKmyfbkq8HvVem4",
+  authDomain: "link-to-the-past-a5042.firebaseapp.com",
+  projectId: "link-to-the-past-a5042",
+  storageBucket: "link-to-the-past-a5042.appspot.com",
+  messagingSenderId: "1050404766506",
+  appId: "1:1050404766506:web:20a98e41de038618205453",
+  measurementId: "G-G25SGGN1GY",
 };
 
 // Initialize Firebase
@@ -18,10 +18,10 @@ let storage = null;
 if (window.location.hostname === "localhost") {
   storage = getStorage();
   connectStorageEmulator(storage, "localhost", 9199);
-  console.log("Connected to Firebase Emulator", storage);
+  console.log("Connected to Firebase Emulator");
 } else {
   storage = getStorage(app);
-  console.log("Connected to Firebase Server", storage);
+  console.log("Connected to Firebase Server");
 }
 export { storage, app };
 // Your web app's Firebase configuration
