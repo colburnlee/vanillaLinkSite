683ca9ccdeed5a919212118eadbd5ef8e9428ccf
Lee Colburn <colburn.lee@gmail.com>
2022-10-11 18:18:43 -1000

    updating yarn. updating cloud functions...

--- functions/index.js
+++ functions/index.js
@@ -6,8 +6,30 @@ admin.initializeApp();
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+  // functions.logger.info("Hello logs!", {structuredData: true});
+  console.log("This is the included text at the url end: ", request.query.text);
+  // Chain is the first 3 characters of the address
+  const chain = request.query.text.slice(0, 3);
+  // Address is the rest of the characters
+  const address = request.query.text.slice(4);
+  // Ask the RTDB for the most recent roundId for the chain/pair
+  admin
+      .database()
+      .ref(`/${chain}/${address}`)
+      .orderByChild("roundId")
+      .limitToLast(1)
+      .once("value")
+      .then((snapshot) => {
+        // If there is a roundId, return it
+        if (snapshot.exists()) {
+          const roundId = Object.values(snapshot.val())[0].roundId;
+          response.send(`Current ${chain}/${address} RTDB roundId: ${roundId}`);
+        } else {
+          // If there is no roundId, return an error
+          response.send(`No roundId found for ${chain}/${address}`);
+        }
+      });
+  response.send(`${request.query.text}. Chain: ${chain} Address: ${address}`);
 });
 
 
